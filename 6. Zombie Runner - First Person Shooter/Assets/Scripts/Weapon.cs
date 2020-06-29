42b8e8cf0ea81ece5edd805974c6a77250dfa564
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-06-29 09:37:35 +0300

    11. Muzzle Flash Implemented

--- 6. Zombie Runner - First Person Shooter/Assets/Scripts/Weapon.cs
+++ 6. Zombie Runner - First Person Shooter/Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
     void Update()
     {
        if(Input.GetButtonDown("Fire1"))
@@ -17,6 +18,18 @@ public class Weapon : MonoBehaviour
     }
 
     private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
     {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
@@ -31,6 +44,5 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
-        
     }
 }
