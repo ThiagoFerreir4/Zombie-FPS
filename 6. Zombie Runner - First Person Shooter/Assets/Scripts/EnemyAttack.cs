bb848aa9f01355e1882796784bedf65b6f2b0ad9
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-07-02 09:54:19 +0300

    17. Animation Events implemented

---
+++ 6. Zombie Runner - First Person Shooter/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float damage = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        Debug.Log("bang bang");
+    }
+}
